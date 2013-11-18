95c4ab8f206041fe2d808af445247ecef5842487
oyegorov <yegorov.oleg@gmail.com>
2013-11-17 22:22:04 -0500

    Settings: added key-value dictionary to export

--- src/com/ell/MemoRazor/export/ExportedData.java
+++ src/com/ell/MemoRazor/export/ExportedData.java
@@ -3,15 +3,17 @@ package com.ell.MemoRazor.export;
 import com.ell.MemoRazor.data.WordGroup;
 
 import java.util.ArrayList;
+import java.util.Hashtable;
 
 public class ExportedData {
     private int versionCode;
-
+    private Hashtable<String, Object> settings;
     private ArrayList<ExportedWordGroup> wordGroups;
 
-    public ExportedData (int versionCode, ArrayList<ExportedWordGroup> wordGroups) {
+    public ExportedData (int versionCode, ArrayList<ExportedWordGroup> wordGroups, Hashtable<String, Object> settings) {
         this.versionCode = versionCode;
         this.wordGroups = wordGroups;
+        this.settings = settings;
     }
 
     public int getVersionCode() {
@@ -28,5 +30,13 @@ public class ExportedData {
 
     public void setWordGroups(ArrayList<ExportedWordGroup> wordGroups) {
         this.wordGroups = wordGroups;
+    }
+
+    public Hashtable<String, Object> getSettings() {
+        return settings;
+    }
+
+    public void setSettings(Hashtable<String, Object> settings) {
+        this.settings = settings;
     }
 }
