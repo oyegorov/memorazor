776f621bd7d225d7457a6d43199a0e218fcf934a
oyegorov <yegorov.oleg@gmail.com>
2013-12-24 19:12:37 -0500

    Force Russian locale when Ukrainian or Belorussian system-wide language is set.

--- src/com/ell/MemoRazor/App.java
+++ src/com/ell/MemoRazor/App.java
@@ -2,17 +2,26 @@ package com.ell.MemoRazor;
 
 import android.app.Application;
 import android.content.Context;
-import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
-import android.preference.PreferenceManager;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.util.DisplayMetrics;
+
+import java.util.Locale;
 
 public class App extends Application {
+    public static final String LOCALE_UA = "uk_UA";
+    public static final String LOCALE_BY = "be_BY";
+    public static final String LOCALE_RU = "ru_RU";
+
     private static Context context;
 
     @Override
     public void onCreate() {
         super.onCreate();
         context = this;
+
+        adjustLocale();
     }
 
     public static Context getContext(){
@@ -34,6 +43,18 @@ public class App extends Application {
         } catch (PackageManager.NameNotFoundException e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
             return -1;
+        }
+    }
+
+    private void adjustLocale() {
+        Resources resources = context.getResources();
+        DisplayMetrics displayMetrics = resources.getDisplayMetrics();
+        Configuration config = resources.getConfiguration();
+
+        String language = config.locale.toString();
+        if (language.equals(LOCALE_UA) || language.equals(LOCALE_BY)) {
+            config.locale = new Locale(LOCALE_RU);
+            resources.updateConfiguration(config, displayMetrics);
         }
     }
 }
